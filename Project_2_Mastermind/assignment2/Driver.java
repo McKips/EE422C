f30a83282f146f46322c1b59e58639a594df81ef
McKips <ktonui@utexas.edu>
2017-06-21 05:16:35 -0500

    Cleaned up program and broke it down into smaller class

--- Project_2_Mastermind/assignment2/Driver.java
+++ Project_2_Mastermind/assignment2/Driver.java
@@ -1,14 +1,14 @@
 package assignment2;
 
 public class Driver {
+
   public static void main (String[] args) {
     boolean testMode = false;
     Game mastermind;
 
-    if (args.length == 0)
-      testMode = false;
-    else if (args[0].equals("1"))
+    if (args.length != 0 && args[0] == "1")
       testMode = true;
+
     mastermind = new Game(testMode);
     mastermind.runGame();
   }
