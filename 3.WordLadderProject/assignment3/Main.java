7453a60c764617026e0d7496553f25a4e3277959
McKips <ktonui@utexas.edu>
2017-06-25 13:24:10 -0500

    bfs working

--- 3.WordLadderProject/assignment3/Main.java
+++ 3.WordLadderProject/assignment3/Main.java
@@ -19,185 +19,168 @@ import java.io.*;
 
 public class Main {
 
-  static ArrayList<String> wordLadder;
-  static ArrayList<Node> visited;
-  static Set<String> dictionary;
-  static Graph graph;
+    static ArrayList<String> wordLadder;
+    static Set<String> dictionary;
+    static HashMap<String,Node> graph;
+    static HashSet<Node> visited;
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    Scanner kb;	// input Scanner for commands
-    PrintStream ps;	// output file, for student testing and grading only
-    // If arguments are specified, read/write from/to files instead of Std IO.
-    if (args.length != 0) {
-      kb = new Scanner(new File(args[0]));
-      ps = new PrintStream(new File(args[1]));
-      System.setOut(ps);			// redirect output to ps
-    } else {
-      kb = new Scanner(System.in);// default input from Stdin
-      ps = System.out;			// default output to Stdout
-    }
-    initialize();
-
-    while(true)
-    {
-      wordLadder = parse(kb);
-      if(wordLadder == null)
-        return;
-      printLadder(wordLadder);
-    }
-
-    // TODO methods to read in words, output ladder
-  }
-
-  public static void initialize() {
-
-    dictionary = makeDictionary();
-    wordLadder = new ArrayList<String>();
-    graph = new Graph();
-    visited = new ArrayList<Node>();
-
-  }
-
-  /**
-   * @param keyboard Scanner connected to System.in
-   * @return ArrayList of Strings containing start word, rungs, and end word. 
-   * If command is /quit, return empty ArrayList. 
-   */
-  public static ArrayList<String> parse(Scanner keyboard) {
-
-    ArrayList<String> words = new ArrayList<String>();
-    String start = new String();
-    String end = new String();
-    boolean invalid = false;
-
-    while(!invalid)
-    {
-      start = keyboard.next();
-      if(start.equals("/quit"))
-        return null;
-
-      end = keyboard.next();
-      if(end.length() != 5)
-        System.out.println("INVALID");
-      else
-        invalid = true;
-
-      words = getWordLadderBFS(start.toUpperCase(),end.toUpperCase());
-      words.add(start);
-      words.add(end);
-    }
-    words.toString().toLowerCase();
-    return words;
-  }
-
-  public static ArrayList<String> getWordLadderDFS(String start, String end) {
-
-    // Returned list should be ordered start to end.  Include start and end.
-    // If ladder is empty, return list with just start and end.
-    // TODO some code
-    // TODO more code
-
-    return null; // replace this line later with real return
-  }
-
-  public static ArrayList<String> getWordLadderBFS(String start, String end) {
-
-    // TODO some code
-    // TODO more code
-
-    graph.addNode(start);
-
-    Node n = graph.find(start);
-    Queue<Node> q = new LinkedList<Node>();
-    q.add(n);
-    visited.add(n);
-
-    while ( !q.isEmpty()){
-      Node u = q.poll();
-      nodeNeighbors(u.word);
-      if(u.word.equals(end)){
-        break;
-      }
-      for (Node v : u.neighbors){
-        if( !visited.contains(v) ){
-          visited.add(v);
-          v.parent = u;
-          q.add(v);
+        Scanner kb;	// input Scanner for commands
+        PrintStream ps;	// output file, for student testing and grading only
+        // If arguments are specified, read/write from/to files instead of Std IO.
+        if (args.length != 0) {
+            kb = new Scanner(new File(args[0]));
+            ps = new PrintStream(new File(args[1]));
+            System.setOut(ps);			// redirect output to ps
+        } else {
+            kb = new Scanner(System.in);// default input from Stdin
+            ps = System.out;			// default output to Stdout
         }
-      }
+        initialize();
+
+        while(true)
+        {
+            wordLadder = parse(kb);
+            if(wordLadder == null)
+                return;
+            printLadder(wordLadder);
+            reset();
+        }
     }
 
-    Node fin = graph.find(end);
-
-    while ( fin != null ){
-      wordLadder.add(fin.word);
-      fin = fin.parent;
+    public static void reset(){
+        wordLadder = new ArrayList<String>();
+        visited = new HashSet<Node>();
+        graph = new HashMap<String,Node>();
+        createGraph();
     }
 
-    Collections.reverse(wordLadder);
+    public static void initialize() {
 
-    return wordLadder; 
-  }
-
-
-  public static void printLadder(ArrayList<String> ladder) {
-
-    if(ladder.size() == 2)
-    {
-      System.out.println("no word ladder can be found between " + ladder.get(0).toLowerCase() + " and " + ladder.get(1).toLowerCase() + ".");
-      return;
-    }
-    else if(ladder.size() > 2)
-    {
-      int length = ladder.size() - 2;
-      System.out.println("a " + length + "-rung word ladder exists between " + ladder.get(length-2) + " and " + ladder.get(length-1) + ".");
-      for(int i = 0; i < ladder.size()-2; i++)
-        System.out.println(ladder.get(i).toLowerCase());
-      return;
+        dictionary = makeDictionary();
+        wordLadder = new ArrayList<String>();
+        graph = new HashMap<String,Node>();
+        visited = new HashSet<Node>();
+        createGraph();
     }
 
-  }
-  // TODO
-  // Other private static methods here
-  private static boolean differByOne(String s1, String s2) {
-    if (s1.length() != s2.length())
-      return false;
+    /**
+     * @param keyboard Scanner connected to System.in
+     * @return ArrayList of Strings containing start word, rungs, and end word. 
+     * If command is /quit, return empty ArrayList. 
+     */
+    public static ArrayList<String> parse(Scanner keyboard) {
 
-    int diff = 0;
-    for (int i = 0; i < s1.length(); i++) {
-      if (s1.charAt(i) != s2.charAt(i) && diff++ >= 1) {
-        return false;
-      }
+        ArrayList<String> words = new ArrayList<String>();
+        String start = new String();
+        String end = new String();
+        boolean invalid = false;
+
+        while(!invalid)
+        {
+            start = keyboard.next().toLowerCase();
+            if(start.equals("/quit"))
+                return null;
+
+            end = keyboard.next().toLowerCase();
+            if(end.length() != 5)
+                System.out.println("INVALID");
+            else
+                invalid = true;
+        }
+        return getWordLadderBFS(start,end);
     }
 
-    return true;
-  }
+    public static ArrayList<String> getWordLadderDFS(String start, String end) {
 
-  public static void nodeNeighbors(String start){
-    for(String dic : dictionary){
-      if(differByOne(start,dic) && !start.equals(dic)){
-        graph.addNode(dic);
-        graph.addNeighbors(start,dic);
-      }
+        // Returned list should be ordered start to end.  Include start and end.
+        // If ladder is empty, return list with just start and end.
+        // TODO some code
+        // TODO more code
+
+        return null; // replace this line later with real return
     }
-  }
+
+    public static ArrayList<String> getWordLadderBFS(String start, String end) {
+
+        // TODO some code
+        // TODO more code
+
+        Node n = graph.get(start);
+        Queue<Node> brit = new LinkedList<Node>();
+        brit.add(n);
+        visited.add(n);
+        while ( !brit.isEmpty() ){
+            Node u = brit.poll();
+            if(u == null)
+                break;
+            if( u.word.equals(end) ){
+                Node fin = graph.get(end);
+                while ( fin != null ){
+                    wordLadder.add(fin.word);
+                    fin = fin.parent;
+                }
+                Collections.reverse(wordLadder);
+                return wordLadder;
+            }
+
+            for (String str : u.neighbors){
+                Node v = graph.get(str);
+                if( !visited.contains(v) ){
+                    visited.add(v);
+                    v.parent = u;
+                    brit.add(v);
+                }
+            }
+        }
+        wordLadder.add(start);
+        wordLadder.add(end);
+        return wordLadder;
+    }
 
 
-  /* Do not modify makeDictionary */
-  public static Set<String>  makeDictionary () {
-    Set<String> words = new HashSet<String>();
-    Scanner infile = null;
-    try {
-      infile = new Scanner (new File("five_letter_words.txt"));
-    } catch (FileNotFoundException e) {
-      System.out.println("Dictionary File not Found!");
-      e.printStackTrace();
-      System.exit(1);
+    public static void printLadder(ArrayList<String> ladder) {
+
+        if(ladder.size() == 2)
+        {
+            System.out.println("no word ladder can be found between " + ladder.get(0).toLowerCase() + " and " + ladder.get(1).toLowerCase() + ".");
+            return;
+        }
+        else if(ladder.size() > 2)
+        {
+            int length = ladder.size();
+            System.out.println("a " + length + "-rung word ladder exists between " + ladder.get(0).toLowerCase() + " and " + ladder.get(length-1).toLowerCase() + ".");
+            for(int i = 0; i < ladder.size(); i++)
+                System.out.println(ladder.get(i).toLowerCase());
+            return;
+        }
+
     }
-    while (infile.hasNext()) {
-      words.add(infile.next().toUpperCase());
+    // TODO
+    // Other private static methods here
+    public static void createGraph(){
+        for(String dic : dictionary){
+            Node newNode = new Node(dic);
+            graph.put(dic.toLowerCase(),newNode);
+        }
     }
-    return words;
-  }
+
+
+    /* Do not modify makeDictionary */
+    public static Set<String>  makeDictionary () {
+        Set<String> words = new HashSet<String>();
+        Scanner infile = null;
+        try {
+            infile = new Scanner (new File("five_letter_words.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Dictionary File not Found!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (infile.hasNext()) {
+            words.add(infile.next().toUpperCase());
+        }
+        return words;
+    }
 }
--- 3.WordLadderProject/assignment3/Node.java
+++ 3.WordLadderProject/assignment3/Node.java
@@ -3,13 +3,25 @@ package assignment3;
 import java.util.*;
 
 public class Node {
-  ArrayList<Node> neighbors;
-  String word;
-  Node parent;
+    ArrayList<String> neighbors;
+    String word;
+    Node parent;
 
-  public Node(String word){
-    this.word = word;
-    neighbors = new ArrayList<Node>();
-    parent = null;
-  }
+    public Node(String word){
+        this.word = word.toLowerCase();
+        neighbors = new ArrayList<String>();
+        parent = null;
+
+        for(int i = 0; i < word.length(); i++){
+            StringBuilder tmp = new StringBuilder(word.toUpperCase());
+            for(char letter = 'A'; letter <= 'Z'; letter++){
+                tmp.setCharAt(i,letter);
+                if( !tmp.toString().equals( word.toUpperCase() ) ){
+                    if(Main.dictionary.contains( tmp.toString().toUpperCase() )){
+                        neighbors.add(tmp.toString().toLowerCase() );
+                    }
+                }
+            }
+        }
+    }
 }
